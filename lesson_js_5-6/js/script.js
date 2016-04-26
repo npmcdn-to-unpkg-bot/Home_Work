f00c5ef93825466860b875960a2950f5dfdad6d7
Anton Garasko <tonius7570@gmail.com>
2016-04-26 21:29:16 +0300

    Добавил ДЗ 5-6

---
+++ lesson_js_5-6/js/script.js
@@ -0,0 +1,88 @@
+function stopwatch(elem) {
+    var time = 0;
+    var interval;
+    var offset;
+
+    function update() {
+        if (this.isOn) {
+            time += delta();
+        }
+        var formattedTime = timeFormatter(time);
+        elem.textContent =formattedTime;
+    }
+
+    function delta() {
+        var  now = Date.now();
+        var timePassed = now - offset;
+        offset = now;
+        return timePassed;
+    }
+
+    function timeFormatter(timeInMilliseconds) {
+        var time = new Date(timeInMilliseconds);
+        var minutes = time.getMinutes().toString();
+        var seconds = time.getSeconds().toString();
+        var milliseconds = time.getMilliseconds().toString();
+
+        if (minutes.length < 2) {
+            minutes = '0' + minutes;
+        }
+
+        if (seconds.length < 2) {
+            seconds = '0' + seconds;
+        }
+
+        while (milliseconds.length < 3) {
+            milliseconds = '0' + milliseconds;
+        }
+
+        return minutes +' : ' + seconds + " . " + milliseconds;
+    }
+    
+    this.isOn = false;
+
+    this.start = function () {
+        if (!this.isOn) {
+            interval = setInterval(update.bind(this), 10);
+            offset = Date.now();
+            this.isOn = true;
+        }
+    };
+
+    this.stop = function () {
+        if (this.isOn) {
+            clearInterval(interval);
+            interval = null;
+            this.isOn = false;
+        }
+    };
+
+    this.reset = function () {
+        if (!this.isOn) {
+            time = 0;
+            update();
+        }
+    }
+}
+
+var timer = document.getElementById('timer');
+var toggleBtn = document.getElementById('toggle');
+var resetBtn = document.getElementById('reset');
+
+var watch = new stopwatch(timer);
+
+toggleBtn.addEventListener('click', function () {
+   if (watch.isOn) {
+       watch.stop();
+       toggleBtn.textContent = 'Start';
+   } else {
+       watch.start();
+       toggleBtn.textContent = 'Stop';
+   }
+});
+
+resetBtn.addEventListener('click', function () {
+   watch.reset();
+});
+
+
